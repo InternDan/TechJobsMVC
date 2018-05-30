5dc4227f8e4a3f5f91df324d6a71902bc3411fe9
InternDan <daniel.leib@gmail.com>
2018-05-29 21:24:48 -0500

    Uploading for class

---
+++ src/TechJobs/Controllers/SearchController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using TechJobs.Models;
+
+namespace TechJobs.Controllers
+{
+    public class SearchController : Controller
+    {
+        private static List<Dictionary<string, string>> jobResults = new List<Dictionary<string, string>>();
+
+        public IActionResult Index()
+        {
+            if (jobResults.Count > 0)
+            {
+                ViewBag.jobs = jobResults;
+            }
+            ViewBag.columns = ListController.columnChoices;
+            ViewBag.title = "Search";
+            return View();
+        }
+
+
+        // TODO #1 - Create a Results action method to process 
+        // search request and display results
+        public IActionResult Results(string searchType, string searchTerm)
+        {
+            jobResults.Clear();
+            jobResults = JobData.FindByColumnAndValue(searchType,searchTerm);
+            if (jobResults.Count == 0)
+            {
+                
+                Dictionary<string, string> resultToAdd = new Dictionary<string, string>();
+                resultToAdd.Add("noResult",searchTerm);
+                jobResults.Add(resultToAdd);
+            }
+            return Redirect("Index");
+        }
+    }
+}
